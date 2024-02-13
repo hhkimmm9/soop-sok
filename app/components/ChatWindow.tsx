b7e11869cd140ab2809b5fba3595937f6479f556
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-13 14:11:54 -0500

    Create 'Chat Window' and 'Message' global components.

---
+++ app/components/ChatWindow.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ChatWindow = () => {
+  return (
+    <>
+      {/* chats */}
+      <div>
+        chat window
+      </div>
+
+      {/* input */}
+      <div></div>
+    </>
+  )
+}
+
+export default ChatWindow
