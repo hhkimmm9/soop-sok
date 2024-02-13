e6e162c62e44cee1150679bf854987c06162498f
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-13 12:23:43 -0500

    Refactor profile buttons into a dedicated component.

--- app/(pages)/user/[id]/profile/page.tsx
+++ app/(pages)/user/[id]/profile/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
+
+import Buttons from './components/Buttons'
 
 const Profile = () => {
+
   return (
     <div className='
       min-h-screen
-      px-12
-      py-36
+      my-36
       flex
       flex-col
       gap-16
@@ -31,24 +32,8 @@ const Profile = () => {
       '>
         은솔공주
       </div>
-      <div className='w-72 flex flex-col gap-8'>
-        <Link href={`/user/${`test`}/profile/edit`}
-          className='
-            border
-            py-3
-            rounded-lg
-            text-center
-        '>프로필 수정</Link>
-
-        <Link href='/user/direct-messages'
-          className='
-            border
-            py-3
-            rounded-lg
-            text-center
-        '>DM 보기</Link>
-      </div>
-
+      
+      <Buttons yourself={true} />
     </div>
   )
 };
