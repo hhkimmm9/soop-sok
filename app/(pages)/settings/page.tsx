081d9a609e0ec309b71a9057d6e513e22d4876b9
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-27 14:50:46 -0500

    Include sign-out button on the settings page.

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,11 +1,29 @@
+'use client';
+
 import Link from 'next/link';
 
+import { auth } from '@/app/lib/firebase/firebase';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
+
 const Settings = () => {
+  const [signOut, loading, error] = useSignOut(auth);
+
+  const handleSignout = async () => {
+    const res = await signOut();
+  };
+
   return (
-    <div>
-      <Link href={`/users/${1}/profile`}>Profile</Link>
+    <div className='flex flex-col gap-4 items-center'>
+      <Link href={`/users/${1}/profile`}
+        className='
+          w-full border rounded-lg p-2 bg-white text-center shadow-sm
+      '>Profile</Link>
+      <button onClick={handleSignout}
+        className='
+          w-full border rounded-lg p-2 bg-white shadow-sm
+      '>Sign Out</button>
     </div>
   )
-}
+};
 
-export default Settings
+export default Settings;
