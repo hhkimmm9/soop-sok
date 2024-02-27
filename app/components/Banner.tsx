e6518d41f6f4908d73cde882939584ee5a1973ba
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-27 18:56:57 -0500

    Added animated effects to the banner component.

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import '@/app/components/Marquee.css';
 
 const Banner = () => {
   return (
-    <div className='border border-black rounded-lg bg-white px-2 py-1'>
-      <p className='truncate'>요즘 잡 마켓 실화냐? 엔트리 레벨 뒤지겠네 진짜 굶어 뒤지것네</p>
+    <div className="bg-white text-black py-2 overflow-hidden border border-black rounded-lg">
+      <div className="marquee w-screen">
+        <span className="inline-block px-4">요즘 잡 마켓 실화냐? 엔트리 레벨 뒤지겠네 진짜 굶어 뒤지것네</span>
+      </div>
     </div>
   )
 }
