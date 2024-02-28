4d24ead21da27f131a16100a55b89be5db18b98c
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-27 19:36:33 -0500

    Refactor CreateChat component.

--- app/components/CreateChat.tsx
+++ app/components/CreateChat.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 const CreateChat = ({
   showCreateChat, setShowCreateChat,
@@ -6,14 +6,14 @@ const CreateChat = ({
   showCreateChat: boolean, setShowCreateChat: any,
 }) => {
   const [title, setTitle] = useState('');
-  const [capacity, setCapacity] = useState(0);
-  const [isPrivate, setIsPrivate] = useState(true);
+  const [capacity, setCapacity] = useState(2);
+  const [isPrivate, setIsPrivate] = useState(false);
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
 
-    console.log('handlesSubmit')
+    console.log('handlesSubmit: ')
 
     setShowCreateChat(false);
   };
@@ -26,18 +26,21 @@ const CreateChat = ({
         <input type="text" id='title' name='title'
           value={ title } onChange={(e) => setTitle(e.target.value)}
           className='
-            border border-black p-1 rounded-lg
+            border border-black px-2 py-1 rounded-lg
         '/>
       </div>
       
       {/* capacity */}
       <div className='flex flex-col gap-3'>
         <label htmlFor="capacity">최대 참여 인원</label>
-        <input type="text" id='capacity' name='capacity'
-          value={ capacity.toString() } onChange={(e) => setCapacity(parseInt(e.target.value))}
-          className='
-            border border-black p-1 rounded-lg
-        '/>
+        <div className='grid grid-cols-6'>
+          <input type="range" id='capacity' name='capacity'
+            min='2' max='5' step='1'
+            value={ capacity.toString() } onChange={(e) => setCapacity(parseInt(e.target.value))}
+            className='col-span-3 border border-black p-1 rounded-lg
+          '/>
+          <span className='col-span-1 col-start-6 text-right mr-4'>{ capacity }</span>
+        </div>
       </div>
       
       {/* isPrivate */}
@@ -46,28 +49,29 @@ const CreateChat = ({
 
         <div className='grid grid-cols-2'>
           <div className='flex gap-2'>
-            <input type="radio" id='private' name='private' value='private' />
-            <label htmlFor="private">Private</label>
-          </div>
-
-          <div className='flex gap-2'>
-            <input type="radio" id='public' name='public' value='public' />
+            <input type="radio" id='public' name='public' value='public'
+              checked={!isPrivate} onChange={() => setIsPrivate(false)}
+            />
             <label htmlFor="public">Public</label>
+          </div>
+          
+          <div className='flex gap-2'>
+            <input type="radio" id='private' name='private' value='private'
+              checked={isPrivate} onChange={() => setIsPrivate(true)}
+            />
+            <label htmlFor="private">Private</label>
           </div>
         </div>
       </div>
 
       {/* password */}
-      { isPrivate && (
-        <div className='flex flex-col gap-2'>
-          <label htmlFor="password">Password</label>
-          <input type="password" id='password' name='password'
-            value={ password } onChange={(e) => setPassword(e.target.value)}
-            className='border border-black p-1 rounded-lg'
-          />
-        </div>
-      )}
-      
+      <div className={`${isPrivate ? 'opacity-100 pointer-events-auto ease-in duration-300' : 'opacity-0 pointer-events-none ease-in duration-300'} flex flex-col gap-2`}>
+        <label htmlFor="password">Password</label>
+        <input type="password" id='password' name='password'
+          value={ password } onChange={(e) => setPassword(e.target.value)}
+          className='border border-black p-1 rounded-lg'
+        />
+      </div>
     </form>
   )
 };
