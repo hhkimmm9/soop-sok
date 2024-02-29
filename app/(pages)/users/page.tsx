2bca959fdd8957d7d845bb473067befbbf427cd4
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-02-29 16:58:06 -0500

    Add Link to navigate from friend list to friend profile page.

--- app/(pages)/users/page.tsx
+++ app/(pages)/users/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import User from '@/app/(pages)/users/(components)/User';
 
 const UserListPage = () => {
   var users = [
@@ -19,28 +19,7 @@ const UserListPage = () => {
   return (
     <div className='flex flex-col gap-2'>
       { users.map((user: any) => (
-        <div key={user._id}
-          className='
-            border border-black bg-white p-4 rounded-lg shadow-sm
-            flex gap-3
-        '>
-          <Image
-            src='https://firebasestorage.googleapis.com/v0/b/chat-platform-for-introv-9f70c.appspot.com/o/IMG_2531.jpeg?alt=media&token=a0566f94-5879-439b-9114-193f3564d378'
-            alt=''
-            width={1324}
-            height={1827}
-            className='
-              object-cover
-              w-12 h-12
-              rounded-full
-          '/>
-
-          <div>
-            {/* last signed in, where they are */}
-            <p className='font-semibold'>{ user.name }</p>
-            <p>status: online</p>
-          </div>
-        </div>
+        <User key={user.id} user={user} />
       )) }
     </div>
   )
