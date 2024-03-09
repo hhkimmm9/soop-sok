332ab826f4a2a3a11949a71243d637f42aa12fc3
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-08 21:19:14 -0500

    Create a new chat room.

--- app/(pages)/chats/[type]/[id]/layout.tsx
+++ app/(pages)/chats/[type]/[id]/layout.tsx
@@ -4,9 +4,7 @@ const ChatsLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='h-full grid grid-rows-12'>
       <Banner />
-      <div className='
-        row-start-2 row-span-11 flex flex-col gap-4
-      '>{ children }</div>
+      <div className='row-start-2 row-span-11'>{ children }</div>
     </div>
   )
 };
