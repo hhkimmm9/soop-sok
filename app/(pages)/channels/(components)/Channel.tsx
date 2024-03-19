a7649602bda8c5ac4d9dd000e885d769cd7693e7
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-19 16:58:03 -0400

    Update the user count when a user enters or leaves a channel.

--- app/(pages)/channels/(components)/Channel.tsx
+++ app/(pages)/channels/(components)/Channel.tsx
@@ -1,20 +1,47 @@
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { auth, db } from '@/app/utils/firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection,
+  doc,
+  query,
+  where,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  updateDoc,
+  serverTimestamp,
+  Unsubscribe
+} from 'firebase/firestore';
 
 import { IChannel } from '@/app/interfaces';
 
-const Channel = ({
-  channelData
-} : {
+type channelProp = {
   channelData: IChannel
-}) => {
+};
+
+const Channel = ({ channelData } : channelProp) => {
+  const router = useRouter();
+
+  const enterChannel = async () => {
+    // update the status of the channel.
+    const channelRef = doc(db, 'channels', channelData.id)
+    await updateDoc(channelRef, {
+      numUsers: channelData.numUsers + 1
+    })
+
+    // redriect to the selected channel page.
+    router.push(`/chats/lobby/${channelData.id}`);
+  };
+
   return (
-    <Link href={`/chats/lobby/${channelData.id}`} className='
+    <div onClick={enterChannel} className='
       border border-black p-3 bg-white rounded-lg
       flex flex-col gap-2
     '>
       <h3>{ channelData.name }</h3>
       <p>현재 참여 인원: { channelData.numUsers } / { channelData.capacity }</p>
-    </Link>
+    </div>
   )
 }
 
--- app/(pages)/chats/[type]/[id]/features/page.tsx
+++ app/(pages)/chats/[type]/[id]/features/page.tsx
@@ -1,15 +1,41 @@
 'use client';
 
 import Link from 'next/link';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
+import { auth, db } from '@/app/utils/firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  serverTimestamp
+} from 'firebase/firestore';
 
 const Page = () => {
   const params = useParams();
+  const router = useRouter();
 
-  var channelId;
-  if (localStorage.getItem('channelId')) {
-    channelId = localStorage.getItem('channelId');
-  }
+  var channelId: string | null = localStorage.getItem('channelId');
+
+  const leaveChat = async () => {
+    const channelRef = doc(db, 'channels', params.id.toString());
+
+    const querySnapshot = await getDoc(channelRef);
+
+    const data = querySnapshot.data()
+    if (data) {
+      await updateDoc(channelRef, {
+        numUsers: data.numUsers - 1
+      });
+    }
+
+    router.push(`${params.type == 'lobby' ?
+      '/channels' : `/chats/lobby/${channelId}`}`
+    )
+  };
 
   return (
     <div className='h-full flex flex-col gap-4'>
@@ -37,12 +63,12 @@ const Page = () => {
               h-min py-8 flex justify-center items-center
               border border-black rounded-lg
           '>User List</Link>
-          { params.type === 'chat' && (
-            <Link href={`/chats/lobby/${channelId}`}
+          { params.type != 'dm' && (
+            <div onClick={leaveChat}
               className='
                 h-min py-8 flex justify-center items-center
                 border border-black rounded-lg
-            '>Leave</Link>
+            '>Leave</div>
           )}
         </div>
       </div>
