e7bab5c566688ad78dee7aa41a71a0d12c8f48b9
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-20 14:52:14 -0400

    Update profile setup.

--- app/(pages)/users/[id]/profile/edit/page.tsx
+++ app/(pages)/users/[id]/profile/edit/page.tsx
@@ -1,35 +1,77 @@
 'use client';
 
-import { useState, useEffect, FormEvent } from 'react';
-import { auth } from '@/app/utils/firebase/firebase';
+import { useState, useEffect } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { auth, db } from '@/app/utils/firebase/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  serverTimestamp
+} from 'firebase/firestore';
 
 const ProfileEdit = () => {
-  const [signedInUser, loading, error] = useAuthState(auth);
-  
-  const [user, setUser] = useState(null);
-  const [profile, setProfile] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [introduction, setIntroduction] = useState('');
+  const [interests, setInterests] = useState<string[]>([]);
+
+  const params = useParams();
+  const router = useRouter();
+
+  const [signedInUser] = useAuthState(auth);
 
   useEffect(() => {
-    // fetch user data 
-  }, [])
+    const fecthUser = async () => {
+      if (signedInUser) {
+        const querySnapshot = await getDoc(
+          doc(db, 'users', signedInUser.uid)
+        );
+        const data = querySnapshot.data();
 
-  const handleUpdate = async (e: FormEvent) => {
+        if (data) {
+          setIntroduction(data.profile.introduction);
+          setInterests(data.profile.profile);
+        }
+      }
+      setLoading(true);
+    };
+
+    fecthUser();
+  }, [signedInUser])
+
+  const handleUpdate = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // const res = await fetch('/api', {
+    if (signedInUser) {
+      const updatedUserRef = doc(db, 'users', params.id.toString());
+      await updateDoc(updatedUserRef, {
+        profile: {
+          introduction,
+        }
+      })
 
-    // });
+      router.push(`/users/${params.id}/profile`);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={(e) => handleUpdate(e)}>
+      <form onSubmit={(e) => handleUpdate(e)} className='flex flex-col gap-2'>
         <div className='flex flex-col gap-2'>
-          <label htmlFor="profile">Profile</label>
-          <input id='profile' type="text"
-            value={profile} onChange={(e) => setProfile(e.target.value)}
+          <label htmlFor="introduction">Introduction</label>
+          <input id='introduction' type="text"
+            value={introduction} onChange={(e) => setIntroduction(e.target.value)}
           />
+        </div>
+        {/* <div className='flex flex-col gap-2'>
+          <label htmlFor="interests">Interests</label>
+        </div> */}
+        <div className='flex justify-end'>
+          <button type='submit' className='bg-white p-2'>Update</button>
         </div>
       </form>
     </div>
