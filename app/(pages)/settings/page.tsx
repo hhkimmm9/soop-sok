6eac2e7bc91bd4dce8cf0f01123b467d06f4827c
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-22 17:26:00 -0400

    Fix settings page redirection with document ID.
    - Updated settings page logic to properly handle redirection using document IDs.

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,32 +1,47 @@
 'use client';
 
-import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { auth, db } from '@/app/utils/firebase/firebase';
 import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
 import {
-  collection,
-  query,
-  where,
-  limit,
-  getDocs,
-  updateDoc,
+  collection, doc,
+  query, where, limit,
+  getDoc, getDocs, updateDoc,
 } from 'firebase/firestore';
 
 const Settings = () => {
+  const [signedInUserId, setSignedInUserId] = useState('');
+
   const [signOut] = useSignOut(auth);
 
   const router = useRouter();
 
   const [signedInUser] = useAuthState(auth);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (signedInUser) {
+        const userQuery = query(collection(db, 'users'), where('uId', '==', signedInUser.uid.toString()));
+        const userSnapshot = await getDocs(userQuery);
+        if (!userSnapshot.empty) {
+          const profileData = userSnapshot.docs[0];
+          setSignedInUserId(profileData.id);
+        }
+      }
+    };
+    fetchUser();
+  }, [signedInUser])
+  
+
   const handleSignout = async () => {
-    if (auth.currentUser) {
-      const q = query(collection(db, 'users'), where('uId', '==', auth.currentUser.uid), limit(1));
-      const querySnapshot = await getDocs(q);
-      if (!querySnapshot.empty) {
-        const userRef = querySnapshot.docs[0].ref;
+    if (signedInUser) {
+      const q = query(collection(db, 'users'), where('uId', '==', signedInUser.uid), limit(1));
+      const userSnapshot = await getDocs(q);
+      if (!userSnapshot.empty) {
+        const userRef = userSnapshot.docs[0].ref;
         await updateDoc(userRef, {
           isOnline: false
         });
@@ -41,7 +56,7 @@ const Settings = () => {
 
   return (
     <div className='flex flex-col gap-4 items-center'>
-      <Link href={`/users/${signedInUser?.uid}/profile`}
+      <Link href={`/users/${signedInUserId}/profile`}
         className='
           w-full border rounded-lg p-2 bg-white text-center shadow-sm
       '>Profile</Link>
