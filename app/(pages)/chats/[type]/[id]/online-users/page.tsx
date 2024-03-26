0f103d52be8ed369a389bd474d924c062a3a5931
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-26 10:21:20 -0400

    Implement fetching the list of joined users within the channel.

--- app/(pages)/chats/[type]/[id]/online-users/page.tsx
+++ app/(pages)/chats/[type]/[id]/online-users/page.tsx
@@ -1,28 +1,46 @@
 'use client';
 
-import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { auth, db } from '@/app/utils/firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection, doc, query,
+  where,
+  getDocs,
+} from 'firebase/firestore';
+
+import { TUser } from '@/app/types';
 
 const OnlineUsersPage = () => {
+  const [activeUsers, setActiveUsers] = useState([]);
+
   const params = useParams();
 
-  var onlineUsers = [
-    {
-      _id: '1',
-      name: 'User 1',
-      status: 1
-    },
-    {
-      _id: '2',
-      name: 'User 2',
-      status: 1
-    },
-    {
-      _id: '3',
-      name: 'User 3',
-      status: 0
-    },
-  ];
+  useEffect(() => {
+    const fetchActiveUsers = async () => {
+      const activeUserContainer: any = [];
+      const activeUserQuery = query(collection(db, 'status_board'),
+        where('cId', '==', params.id)
+      );
+      const activeUserSnapshot = await getDocs(activeUserQuery);
+
+      if (!activeUserSnapshot.empty) {
+        activeUserSnapshot.forEach((doc) => {
+          activeUserContainer.push({
+            id: doc.id,
+            ...doc.data()
+          })
+        })
+        // console.log(activeUserContainer)
+        setActiveUsers(activeUserContainer)
+      }
+    };
+    fetchActiveUsers()
+  }, [params.id]);
   
   return (
     <div className='h-full flex flex-col gap-4'>
@@ -32,13 +50,25 @@ const OnlineUsersPage = () => {
         flex flex-col gap-4
       '>
         <ul className='flex flex-col gap-2'>
-          { onlineUsers.map((onlineUser: any) => (
-            <li key={onlineUser._id} className='
+          { activeUsers.map((activeUser: any) => (
+            <li key={activeUser.id} className='
               border border-black p-2 rounded-lg
-              flex justify-between
+              flex items-center justify-between
             '>
-              <p>{ onlineUser.name }</p>
-              <p>{ onlineUser.status }</p>
+              <div className='flex items-center gap-3'>
+                <Image src={activeUser.profilePicUrl} alt='Profile Picture'
+                  width={52} height={52}
+                  className='rounded-full'
+                />
+
+                <p>{ activeUser.displayName }</p>
+              </div>
+
+              {/* TODO: uId here is not their document id. */}
+              <Link href={`/users/${activeUser.uId}/profile`} className='
+                mr-4 border px-2 py-1 rounded-lg
+              '>Profile</Link>
+
             </li>
           )) }
         </ul>
@@ -48,7 +78,7 @@ const OnlineUsersPage = () => {
         className='
         w-full py-2 bg-white
         border border-black rounded-lg shadow-sm text-center
-      '>Go Back</Link>
+      '>Cancel</Link>
     </div>
   )
 };
