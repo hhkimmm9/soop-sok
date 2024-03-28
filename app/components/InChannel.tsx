4f28c12e7036a309cd49493f196c9031cc894fbe
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-03-27 20:53:54 -0400

    Convert channels pages into components.

--- app/components/InChannel.tsx
+++ app/components/InChannel.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect } from 'react';
+import { auth, db } from '@/app/utils/firebase/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  collection,
+  getDocs,
+} from 'firebase/firestore';
+
+import Channel from '@/app/components/Channel';
+
+import { TChannel } from '@/app/types';
 
 const InChannel = () => {
-  return (
-    <div>InChannel</div>
-  )
-}
+  const [channels, setChannels] = useState<TChannel[]>([]);
+  
+  // const [signedInUser, loading, error] = useAuthState(auth);
 
-export default InChannel
+  useEffect(() => {
+    fetchChannels();
+  }, [])
+
+  const fetchChannels = async () => {
+    var channels: TChannel[] = [];
+    try {
+      const querySnapshot = await getDocs(collection(db, 'channels'));
+      querySnapshot.forEach((doc) => {
+        channels.push({
+          id: doc.id,
+          ...doc.data()
+        } as TChannel);
+      });
+      setChannels(channels);
+    } catch (err) {
+      console.error(err);
+    };
+  };
+
+  return (
+    <div className='flex flex-col gap-2'>
+      { channels.map(channel => (
+        <Channel key={channel.id} channelData={channel} />  
+      )) }
+    </div>
+  )
+};
+
+export default InChannel;
