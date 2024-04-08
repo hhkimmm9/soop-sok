5754c3e7f50ba55925d492e5298b92734d4fc180
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-04-08 11:29:42 -0400

    Cleanup the signin logic.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 'use client';
 
-import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAppState } from '@/utils/AppStateProvider';
+import Image from 'next/image';
 import { auth, db } from '@/utils/firebase';
 import { GoogleAuthProvider } from 'firebase/auth';
-import * as firebaseui from 'firebaseui'
-import 'firebaseui/dist/firebaseui.css'
-import {
-  useAuthState,
-  useSignInWithGoogle,
-  useSignInWithApple
-} from 'react-firebase-hooks/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import {
   doc,
   setDoc, getDoc, updateDoc,
@@ -20,71 +13,60 @@ import {
 import Cookies from 'universal-cookie';
 
 export default function Home() {
-  const [signInWithGoogle, user, loadingGoogle, errorGoogle] = useSignInWithGoogle(auth);
-  // const [signInWithApple, loadingApple, errorApple] = useSignInWithApple(auth);
-
-  const [signedInUser, loading, error] = useAuthState(auth);
-
   const router = useRouter();
-
-  const { dispatch } = useAppState();
 
   const cookies = new Cookies();
 
   // Initialize the FirebaseUI Widget using Firebase.
-  var ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
-  ui.start('#firebaseui-auth-container', {
+  const uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: (authResult: any) => {
+        // store the auth token into the cookie
+        // https://www.npmjs.com/package/cookies
+        cookies.set('auth-token', authResult.credential.accessToken);
+
+        const userRef = doc(db, 'users', authResult.user.uid);
+        getDoc(userRef)
+          .then((querySnapshot) => {
+            // if this is the first time sign in, create a new user data and store it.)
+            if (!querySnapshot.exists()) {
+              setDoc(doc(db, 'users', authResult.user.uid), {
+                createdAt: serverTimestamp(),
+                displayName: authResult.user.displayName,
+                email: authResult.user.email,
+                friendWith: [],
+                honourPoints: 0,
+                isEmailVerified: true,
+                isOnline: true,
+                lastLoginTime: serverTimestamp(),
+                photoURL: authResult.user.photoURL,
+                profile: {
+                  introduction: '',
+                  interests: []
+                },
+                uid: authResult.user.uid
+              });
+            }
+            // otherwise, update the isOnline status
+            else {
+              updateDoc(userRef, {
+                isOnline: true,
+                lastLoginTime: serverTimestamp()
+              });
+            };
+            router.push('/components');
+          })
+          .catch((err) => {
+            console.error('Error getting document:', err);
+          })
+          return true;
+      },
+    },
+    signinFlow: 'popup',
+    signInSuccessUrl: '/components',
     signInOptions: [
       GoogleAuthProvider.PROVIDER_ID
     ]
-  });
-
-  const handleSignIn = async () => {
-    const result = await signInWithGoogle();
-
-    // store the auth token into the cookie
-    // https://www.npmjs.com/package/cookies
-    cookies.set('auth-token', result?.user.refreshToken);
-
-    // check if the user is signed in
-    if (result && auth.currentUser) {
-      // if their profile isn't found in the database, create a new one
-      const userRef = doc(db, 'users', auth.currentUser.uid);
-      const querySnapshot = await getDoc(userRef);
-
-      // if this is the first time sign in, create a new user data and store it.)
-      if (!querySnapshot.exists()) {
-        await setDoc(doc(db, 'users', auth.currentUser.uid), {
-          createdAt: serverTimestamp(),
-          displayName: result.user.displayName,
-          email: result.user.email,
-          friendWith: [],
-          honourPoints: 0,
-          isEmailVerified: true,
-          isOnline: true,
-          lastLoginTime: serverTimestamp(),
-          photoURL: result.user.photoURL,
-          profile: {
-            introduction: '',
-            interests: []
-          },
-          uid: auth.currentUser.uid
-        });
-      }
-      // otherwise, update the isOnline status
-      else {
-        const userSnapshot = doc(db, 'users', querySnapshot.id);
-        await updateDoc(userSnapshot, {
-          isOnline: true,
-          lastLoginTime: serverTimestamp()
-        });
-      }
-
-      if (!errorGoogle) {
-        router.push('/components');
-        dispatch({ type: 'SET_TO_CHANNEL' });
-      }
-    }
   };
 
   return (
@@ -93,16 +75,15 @@ export default function Home() {
         <h1 className='text-4xl'>숲 속</h1>
         <p className=''>Lorem, ipsum lor sit amet consectetur adipisicing elit. Doloribus laboriosam dolor maxime suscipit tempore corrupti odit. Assumenda molestias nostrum voluptatem?</p>
       </div>
-
-      { !signedInUser ? (
-        <div className='flex flex-col gap-4 text-center'>
-          <div onClick={handleSignIn}>
-            <div id='firebaseui-auth-container'></div>
-          </div>
-        </div>
-      ) : (
-        <p>signed in</p>
-      )}
+      <div className='z-10 flex flex-col gap-4 text-center'>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+      </div>
+      <Image src='https://firebasestorage.googleapis.com/v0/b/chat-platform-for-introv-9f70c.appspot.com/o/Untitled_Artwork.png?alt=media&token=6b57b2e1-c921-47ff-ae70-9cc2e96ea6c9'
+        alt='background image'
+        width={320}
+        height={320}
+        className='absolute bottom-0 w-full'
+      />
     </div>
   );
 }
