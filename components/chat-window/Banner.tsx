f942c5a0f927a52d235eacbbe08ff0a8260b7cc0
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-04-29 21:08:50 -0400

    Fetch selected banner.

--- components/chat-window/Banner.tsx
+++ components/chat-window/Banner.tsx
@@ -1,6 +1,43 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useAppState } from '@/utils/AppStateProvider';
+import { auth, db } from '@/utils/firebase';
+import { collection, query, getDocs, where, } from 'firebase/firestore';
+import { FirestoreTimestamp } from '@/types';
+
 import '@/components/Marquee.css';
 
+type TBanner = {
+  cid: string;
+  content: string;
+  createdAt: FirestoreTimestamp;
+  selected: boolean;
+  tagOptions: string[];
+};
+
 const Banner = () => {
+  const [banner, setBanner] = useState<TBanner | null>(null);
+
+  useEffect(() => {
+    const fetchBanner = async () => {
+      try {
+        const q = query(collection(db, 'banners'),
+          where('selected', '==', true)
+        );
+  
+        const bannerSnapshop = await getDocs(q);
+        if (!bannerSnapshop.empty) {
+          const selectedBanner = bannerSnapshop.docs[0].data() as TBanner;
+          setBanner(selectedBanner);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchBanner();
+  }, []);
+
   return (
     <div className="
       h-min mt-1 py-2 overflow-hidden
@@ -8,7 +45,7 @@ const Banner = () => {
       text-black  
     ">
       <div className="marquee w-screen">
-        <span className="inline-block px-4">요즘 잡 마켓 실화냐? 엔트리 레벨 뒤지겠네 진짜 굶어 뒤지것네</span>
+        <span className="inline-block px-4">{ banner?.content }</span>
       </div>
     </div>
   )
