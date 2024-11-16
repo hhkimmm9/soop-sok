306bd42e1f4782cf7c2c07f902a7ec65a239e710
Harrison Kim <29724111+hhkimmm9@users.noreply.github.com>
2024-11-15 21:50:28 -0500

    Refactor PUT request handling in channels API: add logging for debugging, improve error responses for missing token and full channels, and streamline member updates on enter/leave actions

--- app/api/channels/[id]/route.ts
+++ app/api/channels/[id]/route.ts
@@ -12,31 +12,40 @@ export async function PUT(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  console.log('Received PUT request');
   const token = getToken(req);
   if (!token) {
+    console.log('No token provided');
     return NextResponse.json({ error: 'No token provided' }, { status: 401 });
   }
 
   const { id } = params;
+  console.log(`Channel ID: ${id}`);
   const { uid } = await req.json();
+  console.log(`User ID: ${uid}`);
   const searchParams = req.nextUrl.searchParams;
   const action = searchParams.get('action');
+  console.log(`Action: ${action}`);
 
   const channelRef = db.collection('channels').doc(id);
   
   try {
     const channelDoc = await channelRef.get();
     const channelData: TChannel = channelDoc.data() as TChannel;
+    console.log('Channel data:', channelData);
 
     if (action === 'enter') {
       if (channelData.numMembers >= channelData.capacity) {
+        console.log('Channel is full');
         return NextResponse.json({ error: 'Channel is full' }, { status: 400 });
       }
     
       const newMembers = [ ...channelData.members, uid ];
       const newNumMembers = channelData.numMembers + 1;
+      console.log('New members:', newMembers);
+      console.log('New number of members:', newNumMembers);
     
-      await channelRef.update(channelRef, {
+      await channelRef.update({
         members: newMembers,
         numMembers: newNumMembers,
         updatedAt: admin.firestore.FieldValue.serverTimestamp()
@@ -44,16 +53,20 @@ export async function PUT(
     } else if (action === 'leave') {
       const newMembers = channelData.members.filter(member => member !== uid);
       const newNumMembers = channelData.numMembers - 1;
+      console.log('New members:', newMembers);
+      console.log('New number of members:', newNumMembers);
     
-      await channelRef.update(channelRef, {
+      await channelRef.update({
         members: newMembers,
         numMembers: newNumMembers,
         updatedAt: admin.firestore.FieldValue.serverTimestamp()
       });
     }
 
+    console.log('Chat updated successfully');
     return NextResponse.json({ message: 'chat updated!' }, { status: 200 });
   } catch (error) {
+    console.error('Error updating chat:', error);
     return NextResponse.json(error, { status: 500 });
   }
 };
